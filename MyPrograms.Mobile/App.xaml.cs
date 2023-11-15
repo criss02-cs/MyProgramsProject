24c0e983522970b240bbaf0552d4e941fb240ef3
CristianoMac <cristianoaloigi0@gmail.com>
2023-11-15 18:26:18 +0100

    Aggiunti progetti mobile e backend

---
+++ MyPrograms.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPrograms.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MyPrograms.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPrograms.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
